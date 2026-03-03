9cc9f8192b9f14a63347c365a8dabfaebdf82285
EvanWee6 <evanhector.wee24@ycdsbk12.ca>
2026-03-02 23:36:31 -0500

    added blueprint files (waiting for implementation)

---
+++ rpg-legends/campaign-persistence/src/main/java/com/github/domain/CombatState.java
@@ -0,0 +1,15 @@
+
+// With the use of AI
+package com.github.domain;
+
+public class CombatState implements CampaignState {
+    @Override
+    public boolean canExit() {
+        return false;
+    }
+
+    @Override
+    public void handleExitRequest() {
+        System.out.println("Cannot exit during battle!");
+        // Return error to UI
+    }
