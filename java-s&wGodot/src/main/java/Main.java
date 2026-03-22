8aff1f2119a6829a705e9b1c7892575dd6209dc6
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2026-03-22 15:56:33 -0400

    reoganized agained

---
+++ java-s&wGodot/src/main/java/Main.java
@@ -0,0 +1,8 @@
+package com.henry;
+
+public class Main {
+    public static void main(String[] args) {
+        // This is the standard way to print to the VS Code console
+        System.out.println("Hello World from VS Code!");
+    }
+}
