16d7a04fcbe1652f455f3791135c403653899c58
EvanWee6 <evanhector.wee24@ycdsbk12.ca>
2026-03-23 14:39:07 -0400

    reformatted to main structure

--- rpg-legends/campaign-persistence/src/main/java/com/github/domain/RoomData.java
+++
@@ -1,7 +0,0 @@
-package com.github.domain;
-
-// With the use of AI
-public class RoomData {
-    public int roomID;
-    public int progress;
-}
---
+++ rpg-legends/src/main/java/com/github/campaign-persistence/domain/PartyData.java
@@ -0,0 +1,11 @@
+package com.github.domain;
+
+import java.util.List;
+import java.util.Map;
+
+// DTOs
+public class PartyData {
+    public Map<String, Integer> heroStats;
+    public List<Integer> levels;
+    public Map<String, Integer> hpManaStatus;
+}
